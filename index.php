74d30af2d07fa0a825db4871a986287f18a02737
dapeeche <vladkuts2001@gmail.com>
2023-11-30 21:10:54 +0200

    DZ_7 complete

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+interface CanEat
+{
+    public function eat();
+}
+
+interface CanFly
+{
+    public function fly();
+}
+
+class Swallow implements CanEat, CanFly
+{
+    public function eat(){}
+    public function fly(){}
+}
+
+class Ostrich implements CanEat
+{
+    public function eat(){}
+}
